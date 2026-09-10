7af110d88e3b7b32df28452afd2e9aae3bf12765
Maksim Nikulin <nikulin-max04-05@yandex.ru>
2026-09-10 14:24:46 +0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MyComponent from "./components/MyComponent.vue";
+import MyNewComponent from "./components/MyNewComponent.vue";
+</script>
+
+<template>
+  <MyComponent />
+  <MyNewComponent/>
+</template>
+
+<style scoped></style>
