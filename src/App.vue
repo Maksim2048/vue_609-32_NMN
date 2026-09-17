43b1d925ddd9248fd4ea574353f89c33c4ebce6a
Maksim Nikulin <nikulin-max04-05@yandex.ru>
2026-09-18 00:09:56 +0500

    lab_2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,75 @@
 <script setup>
-import MyComponent from "./components/MyComponent.vue";
-import MyNewComponent from "./components/MyNewComponent.vue";
+import { onMounted, ref } from "vue";
+import axios from "axios";
+import ProductList from "./components/ProductList.vue";
+
+const items = ref([]);
+const fetchItems = async () => {
+  try {
+    const { data } = await axios.get("https://def6150a58d68352.mokky.dev/items");
+    items.value = data.map((obj) => ({
+      ...obj,
+      isFavorite: false,
+      isAdded: false,
+    }));
+  } catch (e) {
+    console.log(e);
+  }
+};
+onMounted(async () => {
+  await fetchItems();
+});
 </script>
 
 <template>
-  <MyComponent />
-  <MyNewComponent/>
+  <body class="flex flex-col justify-between h-dvh">
+    <div class="mx-auto w-full mb-25 px-4">
+      <nav class="py-3 border-b border-b-slate-200">
+        <div class="flex items-center justify-between">
+          <div class="flex items-center gap-3">
+            <img src="/logo.svg" alt="" />
+            <p class="text-sm">
+              Лучшие цены <br />
+              в интернет-магазинах
+            </p>
+          </div>
+          <ul class="flex gap-10">
+            <li>
+              <a href="" class="flex gap-3 items-center">
+                <span class="p-3.5 rounded-xl bg-slate-100 hover:bg-slate-200 transition">
+                  <img src="/cart-icon.svg" alt="" />
+                </span>
+                Корзина
+              </a>
+            </li>
+            <li>
+              <a href="" class="flex gap-3 items-center">
+                <span class="p-3.5 rounded-xl bg-slate-100 hover:bg-slate-200 transition">
+                  <img src="/like-outline.svg" alt="" />
+                </span>
+                Избранное
+              </a>
+            </li>
+            <li>
+              <a href="" class="flex gap-3 items-center">
+                <span class="p-3.5 rounded-xl bg-slate-100 hover:bg-slate-200 transition">
+                  <img src="/orders-icon.svg" alt="" />
+                </span>
+                Заказы
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main class="pt-10">
+        <h1 class="text-[40px] font-bold mb-5">Каталог</h1>
+        <product-list :items="items"></product-list>
+      </main>
+    </div>
+    <footer class="p-6 bg-slate-100">
+      <div class="text-center text-slate-500">Copyright © 2023 Behoof, Inc. Все права защищены</div>
+    </footer>
+  </body>
 </template>
 
 <style scoped></style>
